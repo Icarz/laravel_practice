accc562826a25d838e3b808165b73445184e4218
Mohammed Rerhaye <Mr.rghay@gmail.com>
2024-07-31 15:02:16 +0100

    the blog posts done!!

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts=Post::all();
+    return view('home',['posts'=>$posts]);
 });
 
 Route::post('/register',[UserController::class,'register']);
@@ -25,4 +27,4 @@ Route::post('/login',[UserController::class,'login']);
 
 
 // blog routes // 
-Route::post('/create-post',[PostController::class,'create-post']);
+Route::post('/create-post',[PostController::class,'createPost']);
